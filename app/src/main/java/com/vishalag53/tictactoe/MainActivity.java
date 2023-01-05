74064172b86795c82649066885dd81eeec5e0170
vishalag53 <vishal.agrawal1207@gmail.com>
2023-01-05 16:53:09 +0530

    Restart button added

--- app/src/main/java/com/vishalag53/tictactoe/MainActivity.java
+++ app/src/main/java/com/vishalag53/tictactoe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.vishalag53.tictactoe;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
@@ -18,8 +19,15 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-
+        Button restartBtn;
         init();
+        restartBtn = findViewById(R.id.restartBtn);
+        restartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newGame(0);
+            }
+        });
     }
 
     private void init(){
@@ -52,39 +60,39 @@ public class MainActivity extends Activity {
                 getString();
                 if(!b1.equals("") && b1.equals(b2) && b2.equals(b3)){
                     Toast.makeText(getApplicationContext(),"Winner is " + b1,Toast.LENGTH_SHORT).show();
-                    newGame();
+                    newGame(5000);
                 }
                 else if(!b4.equals("") && b4.equals(b5) && b5.equals(b6)){
                     Toast.makeText(getApplicationContext(),"Winner is " + b4,Toast.LENGTH_SHORT).show();
-                    newGame();
+                    newGame(5000);
                 }
                 else if(!b7.equals("") && b7.equals(b8) && b8.equals(b9)){
                     Toast.makeText(getApplicationContext(),"Winner is " + b7,Toast.LENGTH_SHORT).show();
-                    newGame();
+                    newGame(5000);
                 }
                 else if(!b1.equals("") && b1.equals(b4) && b4.equals(b7)){
                     Toast.makeText(getApplicationContext(),"Winner is " + b1,Toast.LENGTH_SHORT).show();
-                    newGame();
+                    newGame(5000);
                 }
                 else if(!b2.equals("") && b2.equals(b5) && b5.equals(b8)){
                     Toast.makeText(getApplicationContext(),"Winner is " + b8,Toast.LENGTH_SHORT).show();
-                    newGame();
+                    newGame(5000);
                 }
                 else if(!b3.equals("") && b3.equals(b6) && b6.equals(b9)){
                     Toast.makeText(getApplicationContext(),"Winner is " + b3,Toast.LENGTH_SHORT).show();
-                    newGame();
+                    newGame(5000);
                 }
                 else if(!b1.equals("") && b1.equals(b5) && b5.equals(b9)){
                     Toast.makeText(getApplicationContext(),"Winner is " + b1,Toast.LENGTH_SHORT).show();
-                    newGame();
+                    newGame(5000);
                 }
                 else if(!b3.equals("") && b3.equals(b5) && b5.equals(b7)){
                     Toast.makeText(getApplicationContext(),"Winner is " + b3,Toast.LENGTH_SHORT).show();
-                    newGame();
+                    newGame(5000);
                 }
                 else if(count==9){
                     Toast.makeText(getApplicationContext(),"Match is Draw",Toast.LENGTH_SHORT).show();
-                    newGame();
+                    newGame(5000);
                 }
             }
         }
@@ -102,7 +110,7 @@ public class MainActivity extends Activity {
         b9 = btn9.getText().toString();
     }
 
-    public void newGame(){
+    public void newGame(int time){
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -118,6 +126,6 @@ public class MainActivity extends Activity {
                 count=0;
                 flag=0;
             }
-        },10000);
+        },time);
     }
 }
